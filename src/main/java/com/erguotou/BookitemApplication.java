af5086219d03c1321ea0d800f4f11e1dfc1f5d4c
SubErguotou <fks12301818@163.com>
2020-08-13 12:22:48 +0800

    bookitem后端springboot初始化成功

---
+++ src/main/java/com/erguotou/BookitemApplication.java
@@ -0,0 +1,13 @@
+package com.erguotou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookitemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookitemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erguotou/BookitemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erguotou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookitemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
